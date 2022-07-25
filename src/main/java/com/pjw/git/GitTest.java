c923dd90a1d4117c86398ef2928ef9486a3367a3
pengjianwang <771683043@qq.com>
2022-07-25 15:46:52 +0800

    second commit

--- src/main/java/com/pjw/git/GitTest.java
+++ src/main/java/com/pjw/git/GitTest.java
@@ -3,5 +3,6 @@ package com.pjw.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git Test");
+        System.out.println("Hello Git Test2");
     }
 }
